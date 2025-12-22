34396df5df829925080afb977fe91184d6923e41
saniowl <saniowlhasan2@gmail.com>
2025-12-22 13:31:21 +0600

    dfdfd

--- main.js
+++ main.js
@@ -292,3 +292,112 @@ style.textContent = `
     }
 `;
 document.head.appendChild(style);
+
+
+
+const blogTitle = document.querySelector(".blog__reveal");
+const blogSection = document.querySelector("#blog");
+
+if (blogTitle) {
+    // Split text into individual letters
+    const blogLetters = blogTitle.textContent.split("");
+    blogTitle.innerHTML = blogLetters.map(l => `<span>${l}</span>`).join("");
+
+    // Animate on scroll
+    gsap.to(".blog__reveal span", {
+        y: 0,
+        duration: 0.6,
+        ease: "power4.out",
+        stagger: 0.12,
+        scrollTrigger: {
+            trigger: blogSection,
+            scroller: "main",
+            start: "top 80%",
+            once: false,
+            onEnter: () => {
+                gsap.set(".blog__reveal span", { y: "110%" });
+            },
+            // markers: true // Uncomment to see trigger points
+        }
+    });
+
+    // Animate out when scrolling past
+    ScrollTrigger.create({
+        trigger: blogSection,
+        scroller: "main",
+        start: "top 20%",
+        end: "top -20%",
+        onLeave: () => {
+            gsap.to(".blog__reveal span", {
+                y: "0",
+                duration: 0.2,
+                ease: "power3.in",
+                stagger: 0.03
+            });
+        },
+        onEnterBack: () => {
+            gsap.to(".blog__reveal span", {
+                y: "100%",
+                duration: 0.2,
+                ease: "power3.out",
+                stagger: 0.03
+            });
+        },
+        // markers: true // Uncomment to see trigger points
+    });
+}
+
+
+const blogPosts = [
+    {
+        title: 'BLOG POST ONE',
+        time: '3 MIN',
+        image: 'https://images.unsplash.com/photo-1561998338-13ad7883b20f?auto=format&fit=crop&q=80&w=2487&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' 
+    },
+    {
+        title: 'BLOG POST TWO',
+        time: '4 MIN',
+        image: 'https://images.unsplash.com/photo-1563089145-599997674d42?auto=format&fit=crop&q=80&w=2370&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' 
+    },
+    {
+        title: 'BLOG POST THREE',
+        time: '5 MIN',
+        image: 'https://images.unsplash.com/photo-1454117096348-e4abbeba002c?auto=format&fit=crop&q=80&w=2602&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' 
+    }
+]
+
+const createBlogposts = () => {
+    blogPosts.forEach(post => {
+        let blogPostSection = document.createElement('div');
+        blogPostSection.classList.add('blog__post');
+
+        let postDiv = document.createElement('div');
+        postDiv.classList.add('post');
+
+        let imageContainer = document.createElement('div');
+        imageContainer.classList.add('post__image__container');
+
+        let image = document.createElement('img');
+        image.classList.add('blog__post__img');
+        image.src = post.image;
+
+        let postDetails = document.createElement('div');
+        postDetails.classList.add('post__details');
+
+        let postTitle = document.createElement('p');
+        postTitle.innerText = post.title;
+
+        let postTime = document.createElement('p');
+        postTime.innerText = post.time;
+
+        imageContainer.appendChild(image);
+        postDetails.append(postTitle, postTime);
+        postDiv.append(imageContainer, postDetails)
+        blogPostSection.appendChild(postDiv);
+
+        document.getElementById('blog').appendChild(blogPostSection)
+
+    })
+}
+
+createBlogposts();
